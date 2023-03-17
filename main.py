468c9bf0d106cee601ec48f6d2df4ff5cafef5be
irinaenescu2002 <irina-stefania.enescu@s.unibuc.ro>
2023-03-17 21:06:46 +0200

    Workflow Diagram

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+    def build(self):
+        return Label(text="test")
+
+if __name__ == "__main__":
+    MyApp().run()
